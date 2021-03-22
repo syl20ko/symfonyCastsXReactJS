fddc28fbdb2e315a7228c0a8fd10ce14267f723b
syl20ko <sylvain.caron@baliztic.com>
2021-03-22 16:04:24 +0100

    SymfonyXReact

---
+++ assets/js/Components/rep_log_react.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import RepLogApp from "../RepLog/RepLogApp";
+
+render(<RepLogApp />, document.getElementById("lift-stuff-app"));
---
+++ assets/js/RepLog/RepLogApp.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+
+class RepLogApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      highlightedRowId: null,
+    };
+  }
+
+  handleRowClick(repLogId) {
+    this.setState({ highlightedRowId: repLogId });
+  }
+
+  render() {
+    const { highlightedRowId } = this.state;
+
+    const repLogs = [
+      { id: 1, reps: 25, itemLabel: "My Laptop", totalWeightLifted: 112.5 },
+      { id: 2, reps: 10, itemLabel: "Big Fat Cat", totalWeightLifted: 180 },
+      { id: 8, reps: 4, itemLabel: "Big Fat Cat", totalWeightLifted: 72 },
+    ];
+
+    return (
+      <div className="col-md-7 js-rep-log-table">
+        <h2>Lift History</h2>
+
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>What</th>
+              <th>How many times?</th>
+              <th>Weight</th>
+              <th>&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            {repLogs.map((repLog) => (
+              <tr
+                key={repLog.id}
+                className={highlightedRowId === repLog.id ? "info" : ""}
+                onClick={() => this.handleRowClick(repLog.id)}
+              >
+                <td>{repLog.itemLabel}</td>
+                <td>{repLog.reps}</td>
+                <td>{repLog.totalWeightLifted}</td>
+                <td>...</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>&nbsp;</td>
+              <th>Total</th>
+              <th>TODO</th>
+              <td>&nbsp;</td>
+            </tr>
+          </tfoot>
+        </table>
+        <form
+          className="form-inline"
+          noValidate
+          data-url="{{ path('rep_log_new') }}"
+        >
+          <div className="form-group">
+            <label
+              className="sr-only control-label required"
+              htmlFor="rep_log_item"
+            >
+              What did you lift?
+            </label>
+            <select
+              id="rep_log_item"
+              name="item"
+              required="required"
+              className="form-control"
+            >
+              <option value="">What did you lift?</option>
+              <option value="cat">Cat</option>
+              <option value="fat_cat">Big Fat Cat</option>
+              <option value="laptop">My Laptop</option>
+              <option value="coffee_cup">Coffee Cup</option>
+            </select>
+          </div>{" "}
+          <div className="form-group">
+            <label
+              className="sr-only control-label required"
+              htmlFor="rep_log_reps"
+            >
+              How many times?
+            </label>
+            <input
+              type="number"
+              id="rep_log_reps"
+              name="reps"
+              required="required"
+              placeholder="How many times?"
+              className="form-control"
+            />
+          </div>{" "}
+          <button type="submit" className="btn btn-primary">
+            I Lifted it!
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RepLogApp;
